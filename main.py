43f3f5687ea268fca0967a32b1a462d79426a08b
Ben Phillips <ben@thread.com>
2022-10-14 12:40:59 +0000

    Wire up the home page

--- main.py
+++ main.py
@@ -16,10 +16,19 @@ app = flask.Flask(
     static_folder='static',
 )
 
+
 @app.route('/')
 @login_required
 def home():
-    return "Home"
+    fitbit = {
+        'has_access_token': f"fitbit_access_token:{flask.g.user_id}" in db,
+        'has_refresh_token': f"fitbit_refresh_token:{flask.g.user_id}" in db,
+    }
+
+    return flask.render_template('home.html', **{
+        'fitbit': fitbit,
+    })
+
 
 @app.route('/login')
 def login():
@@ -28,9 +37,11 @@ def login():
 
     return flask.render_template('login.html')
 
+
 @app.before_request
 def load_user():
     flask.g.user_id = flask.request.headers.get('X-Replit-User-Id')
+
 
 # Read access token from DB
 # Attempt to connect
@@ -41,13 +52,14 @@ def load_user():
 # Attempt to connect
 # If fail, error with message
 
-
 #access_token = db.get('access_token')
 
 #if not access_token:
 #    refresh_access_token()
 
+
 @app.route('/redirect')
+@login_required
 def handle_redirect_from_fitbit():
     code = flask.request.args.get('code')
     if not code:
@@ -61,26 +73,28 @@ def handle_redirect_from_fitbit():
         "code_verifier": db[f"code_verifier:{flask.g.user_id}"],
         "grant_type": "authorization_code",
     }
-    
+
     url = "https://api.fitbit.com/oauth2/token"
     response = requests.post(url, data=params)
 
     data = response.json()
 
-    db[f"access_token:{flask.g.user_id}"] = data['access_token']
-    db[f"refresh_token:{flask.g.user_id}"] = data['refresh_token']
-    
+    db[f"fitbit_access_token:{flask.g.user_id}"] = data['access_token']
+    db[f"fitbit_refresh_token:{flask.g.user_id}"] = data['refresh_token']
+
     return "Authorised."
 
+
 @app.route('/authorise')
+@login_required
 def request_authorisation():
     code_verifier = secrets.token_urlsafe(43)
     db[f"code_verifier:{flask.g.user_id}"] = code_verifier
 
     code_challenge = base64.urlsafe_b64encode(
-        hashlib.sha256(code_verifier.encode("utf-8")).digest()
-    ).decode("utf-8")[:-1]
-    
+        hashlib.sha256(
+            code_verifier.encode("utf-8")).digest()).decode("utf-8")[:-1]
+
     params = {
         "client_id": os.environ["FITBIT_CLIENT_ID"],
         "response_type": "code",
@@ -88,11 +102,13 @@ def request_authorisation():
         "code_challenge_method": "S256",
         "scope": "weight",
     }
+
+    authorization_url = "https://www.fitbit.com/oauth2/authorize?" + urlencode(
+        params)
     
-    authorization_url = "https://www.fitbit.com/oauth2/authorize?" + urlencode(params)
     return flask.redirect(authorization_url)
-    
-    
+
+
 if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
